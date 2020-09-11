259d771c194f19135b506619f269b1b2b6c38e7e
Jessica <jbjb2200@gmail.com>
2020-09-10 20:56:34 -0700

    Added comments

--- Deck.java
+++ Deck.java
@@ -7,6 +7,7 @@
 /**
  *
  * @author jbjb2
+ * The Deck class represents 52 playing cards, which makes up a typical deck.
  */
 import java.util.*;
 
@@ -14,6 +15,7 @@ public class Deck {
     private LinkedList<Card> deckOfCards;
     private int deckSize;
     
+    // constructs a deck with the usual frequency of suits and numbers
     public Deck() {
         deckOfCards = new LinkedList<Card>();
         
@@ -29,32 +31,37 @@ public class Deck {
         deckSize = deckOfCards.size();
     }
     
+    // constructs a deck from the given LinkedList
     public Deck(LinkedList<Card> d) {
         deckOfCards = d;
         
         deckSize = d.size();
     }
     
+    // returns how many cards are in the deck
     public int getDeckSize() {
         return deckSize;
     }
     
+    // shuffles deck using shuffle method in Collections
     public void shuffleVers1() {
         Collections.shuffle(deckOfCards);
     }
     
+    // shuffles deck using random method
     public void shuffleVers2() {
         for (int count = 0; count < deckSize; count++) {
             deckOfCards.add((int)(Math.random() * deckSize), deckOfCards.removeFirst());
         }
     }
     
+    // returns a card on top of deck
     public Card drawFirst() {
         deckSize--;
         return deckOfCards.removeFirst();
     }
     
-    //work on !!
+    // work on !!
     /*
     public LinkedList<Card> drawMultiple(int count) {
         LinkedList<Card> removals = new LinkedList<Card>();
@@ -68,12 +75,13 @@ public class Deck {
         return removals;
     }*/
     
-    
+    // adds given card to bottom of deck
     public void addLast(Card c) {
         deckSize++;
         deckOfCards.addLast(c);
     }
     
+    // adds LinkedList of cards to bottom of deck
     public void addManyLast(LinkedList<Card> cards) {
         deckSize += cards.size();
         
@@ -82,6 +90,7 @@ public class Deck {
         }
     }
     
+    // returns top half of deck
     public LinkedList<Card> getFirstHalf() {
         int half = deckSize / 2;
         
@@ -95,6 +104,7 @@ public class Deck {
         return halfDeck;
     }
     
+    // returns bottom half of deck
     public LinkedList<Card> getLastHalf() {
         int half = deckSize / 2;
         
@@ -107,6 +117,7 @@ public class Deck {
         return halfDeck;
     }
     
+    // prints out all of the cards in the deck
     public String toString() {
         String msg = "";
         
